a55efaa26f602bacee961d62ce894553fec5eaa0
Zach W <zach39908@gmail.com>
2023-07-01 20:59:25 -0700

    Add script files

---
+++ scripts/month.js
@@ -0,0 +1,3 @@
+// Display current year
+today = new Date();
+document.querySelector('.prevPage').textContent = today.getFullYear();
---
+++ scripts/year.js
@@ -0,0 +1,3 @@
+// Display current year as title
+today = new Date();
+document.querySelector('h1.title').textContent = today.getFullYear();
