ecc2445b0e8f1b6454b1577fdbb2e811bb5364c8
Zach <zach39908@gmail.com>
2023-07-05 14:42:41 -0700

    Display year and month for index.html, month.html

--- scripts/month.js
+++ scripts/month.js
@@ -1,3 +1,5 @@
-// Display current year
+// Display current year and month
 today = new Date();
 document.querySelector('.prevPage').textContent = today.getFullYear();
+document.querySelector('.title').textContent = localStorage.getItem('month');
+
--- scripts/year.js
+++ scripts/year.js
@@ -1,3 +1,9 @@
 // Display current year as title
 today = new Date();
 document.querySelector('h1.title').textContent = today.getFullYear();
+
+// Save selected month to local storage
+document.querySelectorAll('li.month').forEach(listItem => listItem.addEventListener('click', () => {
+    const itemText = listItem.children.item(0).textContent;
+    localStorage.setItem('month', itemText);
+}));
