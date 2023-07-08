1c959bc8b701e2bff5a90b1dad0a4d65e944b4fc
Zach W <zach39908@gmail.com>
2023-07-07 20:52:32 -0700

    Display weekday on day.html page

--- scripts/day.js
+++ scripts/day.js
@@ -1,3 +1,9 @@
 // Display current year
 const today = new Date();
-document.querySelector('.prevPage').textContent = today.getFullYear();
+const headerLinks = Array.from(document.querySelectorAll('.prevPage'));
+headerLinks[0].textContent = today.getFullYear();
+const month = localStorage.getItem('month');
+const day = localStorage.getItem('day');
+const weekday = localStorage.getItem('weekday');
+document.querySelector('.title').textContent = `${month} ${day}`;
+headerLinks[1].textContent = weekday;
--- scripts/month.js
+++ scripts/month.js
@@ -2,8 +2,8 @@
 const today = new Date();
 const thisYear = today.getFullYear();
 document.querySelector('.prevPage').textContent = thisYear; // set year text on page
-const selectedMonth = localStorage.getItem('month');
-document.querySelector('.title').textContent = selectedMonth; // set month text on page
+const month = localStorage.getItem('month');
+document.querySelector('.title').textContent = month; // set month text on page
 
 // Add extra row of cells for wide-screen devices
 if(window.screen.width >= 750) {
@@ -18,22 +18,29 @@ if(window.screen.width >= 750) {
 }
 
 // Add date numbers and weekdays to cells in calendar
-const getOffset = new Date(`${selectedMonth} 1, ${today.getFullYear()} 00:00:00`);
+const getOffset = new Date(`${month} 1, ${today.getFullYear()} 00:00:00`);
 const weekdayOffset = getOffset.getDay(); // get offset (weekday of the current month's first day)
 const getDays = new Date(today.getFullYear(), today.getMonth(), 0);
 const daysInMonth = parseInt(getDays.getDate()) + 1; // get total days in current month
 const dayCells = Array.from(document.querySelectorAll('li.day')); // array of day cells (list items)
-const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const weekdays = ['Sunday', 'Monday', 'Tueday', 'Wedday', 'Thursday', 'Friday', 'Saturday'];
 
 if(window.screen.width >= 750) { // use offset for wide-screen devices
-    for(let i=0; i < daysInMonth; i++) {
-        dayCells[i + weekdayOffset].dataset.number = `${i+1}`; // add day number to each cell
+    for(let i = weekdayOffset; i < daysInMonth + weekdayOffset; i++) {
+        dayCells[i].dataset.number = `${i - weekdayOffset + 1}`; // add day number to each cell
+        const getWeekday = new Date(`${month} ${i - weekdayOffset +  1}, ${today.getFullYear()} 00:00:00`);
+        dayCells[i].dataset.weekday = weekdays[getWeekday.getDay()].slice(0, 3); // add weekday to each cell (not visible)
     }
 }
 else { // mobile screens disregard offset
     for(let i=0; i < daysInMonth; i++) {
         dayCells[i].dataset.number = `${i+1}`; // add day number to each cell
-        const getWeekday = new Date(`${selectedMonth} ${i+1}, ${today.getFullYear()} 00:00:00`);
-        dayCells[i].dataset.weekday = weekdays[getWeekday.getDay()];
+        const getWeekday = new Date(`${month} ${i + 1}, ${today.getFullYear()} 00:00:00`);
+        dayCells[i].dataset.weekday = weekdays[getWeekday.getDay()].slice(0, 3); // add weekday to each cell
     }
 }
+
+document.querySelectorAll('li.day').forEach(day => day.addEventListener('click', () => {
+    localStorage.setItem('day', day.dataset.number);
+    localStorage.setItem('weekday', weekdays.find(elem => elem.slice(0, 3) === day.dataset.weekday));
+}));
